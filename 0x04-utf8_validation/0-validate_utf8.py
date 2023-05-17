1f8876e98866335b485a14ef47c79c35f757f928
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-05-17 15:39:46 +0300

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,39 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
 def validUTF8(data):
-    # Number of bytes to validate for the current character
-    num_bytes = 0
+    """Determines if a given data set
+    represents a valid utf-8 encoding
+    """
+    number_bytes = 0
 
-    for byte in data:
-        # Check if the current byte is a continuation byte
-        if num_bytes > 0 and (byte >> 6) == 0b10:
-            num_bytes -= 1
-        # Check if the current byte is a starting byte
-        elif num_bytes == 0:
-            # Count the number of leading 1s to determine the number of bytes
-            mask = 0b10000000
-            while mask & byte:
-                num_bytes += 1
-                mask >>= 1
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
 
-            # Handle invalid cases
-            if num_bytes == 1 or num_bytes > 4:
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-        # Invalid case: current byte is not a continuation byte
-        else:
-            return False
 
-    # Check if all expected continuation bytes are present
-    return num_bytes == 0
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
